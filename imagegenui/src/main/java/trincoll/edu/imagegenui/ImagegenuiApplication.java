7f95d5e4320f98ed255ad545e07c43d5e82b9e69
Mayonezyck <71305052+Mayonezyck@users.noreply.github.com>
2023-04-02 15:31:25 -0400

    spring files

---
+++ imagegenui/src/main/java/trincoll/edu/imagegenui/ImagegenuiApplication.java
@@ -0,0 +1,13 @@
+package trincoll.edu.imagegenui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagegenuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagegenuiApplication.class, args);
+	}
+
+}
---
+++ imagegenui/src/test/java/trincoll/edu/imagegenui/ImagegenuiApplicationTests.java
@@ -0,0 +1,13 @@
+package trincoll.edu.imagegenui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImagegenuiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
